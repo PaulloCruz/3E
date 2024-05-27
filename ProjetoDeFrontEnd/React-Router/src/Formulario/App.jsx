d865ab166d3ed7e27476164018b01e78af0d4a42
paullo.cruz@hotmail.com <paullo.cruz@hotmail.com>
2024-05-27 12:40:17 -0300

    aula 27.05

--- ProjetoDeFrontEnd/React-Router/src/Formulario/App.jsx
+++ ProjetoDeFrontEnd/React-Router/src/Formulario/App.jsx
@@ -1,37 +1,44 @@
 import React from "react"
 
-const App = ()=>{
-
-    const [cep, setCep] = React.useState()
-    const [error, setError]= React.useState(null)
-cd 
-
-    function validaCep(value){
-        event.preventDefault()
-        if(value === undefined){
-            setError("O campo não pode está vazio!")
-            return false
-        }
-        else{
-            setError(null)
-            return true
-        }
+const App = () => {
+  const [userName, setUserName] = React.useState("")
+  const [password, setPassword] = React.useState("")
+  const [error, setError] = React.useState(null)
+  const validationField = (value) => {
+    if (value == "") {
+      setError("campo inválido!")
+      return false 
+    } else {
+      setError(null)
+      return true
     }
-    return(
-        <form onSubmit={validaCep}>
-            <label htmlFor="cep">CEP</label>
-            <input 
-            type="text"
-            id="cep"
-            value={cep}
-            onBlur={handleBlur}
-            onChange={({target})=> setCep(target.value)}
-            
-            />
-            <button>Enviar  </button>
-        </form>
-    )
+  }
 
+  const validedFields = (err, ...values) => {
+    const allValues = values.some(e => e !== "")
+    console.log(err, allValues)
+    if (err != null && allValues == false) {
+      setError("preencha todos os campos corretamente")
+      return false
+    } else {
+      setError(null)
+      return true
+    }
+  }
+  
+  return (
+    <form onSubmit={(Event) => { 
+      Event.preventDefault()
+      validedFields(error, userName)
+    }} className="forms" action="">
+      <label htmlFor="userName">Nome:</label>
+      <input type="text" id="userName" placeholder="Digite seu nome" onBlur={({target}) => validationField(target.value)} value={userName} onChange={({ target }) => setUserName(target.value)} />
+      <label htmlFor="password">Senha:</label>
+      <input type="text" id="password" placeholder="Digite sua senha" onBlur={({target}) => validationField(target.value)} value={password} onChange={({ target }) => setPassword(target.value)} />
+      {error && <span>{error}</span>}
+      <button>Enviar</button>
+    </form>
+  )
 }
 
 export default App
